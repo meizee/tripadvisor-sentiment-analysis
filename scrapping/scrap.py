30983bb8336f8dc6bbfd52979fc731bb3c9013e6
Ivan Muhammad Nizar <ivannizar@gmail.com>
2022-01-05 13:28:53 +0700

    fix bug span

--- scrapping/scrap.py
+++ scrapping/scrap.py
@@ -7,7 +7,7 @@ from time import sleep
 driver = webdriver.Chrome(ChromeDriverManager().install())
 driver.implicitly_wait(30)
 # https://www.tripadvisor.com/Attractions-g297698-Activities-Nusa_Dua_Nusa_Dua_Peninsula_Bali.html
-url = "https://www.tripadvisor.com/Attractions-g297698-Activities-oa*-place_state.html" 
+url = "https://www.tripadvisor.com/Attractions-g297698-Activities-oa*-place_state.html" #ganti g....
 # Nusa_Dua_Nusa_Dua_Peninsula_Bali
 place="Nusa_Dua" #You can replace your place here.
 state="Nusa_Dua_Peninsula_Bali"       #You can replace your state here.
@@ -15,7 +15,7 @@ url = url.replace("place",place)
 url = url.replace("state",state)
 links=[]
 location_names=[]
-for i in range(0,30,10):
+for i in range(0,15,10):
     target_url=url.replace("*",str(i))
     driver.get(target_url)
     bsobj = BeautifulSoup(driver.page_source, 'html.parser')
@@ -41,11 +41,14 @@ reviews_location=[]
 rating_list = []
 date_list = []
 count=0
+# print(links)
+# for word in links:
+#     print(word)
 for link in links:
     location=location_names[count]
     count=count+1
     flag=0
-    for i in range(5,20,5):
+    for i in range(0,40,5):
         target_link=link.format(i)
         print(target_link)
         html2 = driver.get(target_link)
@@ -53,7 +56,7 @@ for link in links:
         reviews_section=bsobj2.find('div',{'class':'dHjBB'})
         if reviews_section is None:
             break
-        reviews_div=reviews_section.findChildren("div",{'class':'WlYyy diXIH dDKKM'} , recursive=True)
+        reviews_div=reviews_section.findChildren("span",{'class':'NejBf'} , recursive=True)
         rating_svg = reviews_section.findChildren("svg", {'class':'RWYkj d H0'}, recursive=True)
         date_div=reviews_section.findChildren("div", {'class':'WlYyy diXIH cspKb bQCoY'}, recursive=True)
 
@@ -64,16 +67,16 @@ for link in links:
       
         for r, rs, d in zip(reviews_div, rating_svg, date_div):
             # print(rs['aria-label'])
-            if r.span is None or rs is None or d is None:
+            if r is None or rs is None or d is None:
                 flag=1
                 break
-            reviews_list.append(str(r.span.text.strip()))
+            reviews_list.append(str(r.text.strip()))
             reviews_location.append(location)
             rating_list.append(rs['title'])
             print(d.text.strip())
             date_list.append(d.text.strip())
             #sleep(1)
-            print(str(r.span.text.strip()))
+            print(str(r.text.strip()))
             print(rs['title'])
         
         dataframe = pd.DataFrame({'location':reviews_location,'content':reviews_list, 'rating':rating_list, 'date':date_list})
