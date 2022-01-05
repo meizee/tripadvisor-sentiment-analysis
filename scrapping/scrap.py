22a812348e785e60646658a2978ba45e0fb57cb7
Ivan Muhammad Nizar <ivannizar@gmail.com>
2022-01-05 18:51:27 +0700

    fix komen dikomen

--- scrapping/scrap.py
+++ scrapping/scrap.py
@@ -7,10 +7,10 @@ from time import sleep
 driver = webdriver.Chrome(ChromeDriverManager().install())
 driver.implicitly_wait(30)
 # https://www.tripadvisor.com/Attractions-g297698-Activities-Nusa_Dua_Nusa_Dua_Peninsula_Bali.html
-url = "https://www.tripadvisor.com/Attractions-g297698-Activities-oa*-place_state.html" #ganti g....
+url = "https://www.tripadvisor.com/Attractions-g608487-Activities-oa*-place_state.html" #ganti g....
 # Nusa_Dua_Nusa_Dua_Peninsula_Bali
-place="Nusa_Dua" #You can replace your place here.
-state="Nusa_Dua_Peninsula_Bali"       #You can replace your state here.
+place="Legian_Kuta" #You can replace your place here.
+state="District_Bali"       #You can replace your state here.
 url = url.replace("place",place)
 url = url.replace("state",state)
 links=[]
@@ -66,19 +66,35 @@ for link in links:
         reviews_section=bsobj2.find('div',{'class':'dHjBB'})
         if reviews_section is None:
             break
-        reviews_div=reviews_section.findChildren("span",{'class':'NejBf'} , recursive=True)
+        # print(reviews_section)
+        # print("=============")
+        reviews_span=[]
+        reviews_div=reviews_section.find_all("div",{'class':'WlYyy diXIH dDKKM'} , recursive=True)
+        for div in reviews_div:
+            span=div.find('span')
+            # print(span)
+            reviews_span.append(span)
+        # print(reviews_span)
+        # print(reviews_div[0].span(class_ = 'NejBf'))
+        # print("==========================================================================")
+        # reviews_div=reviews_section.find_all("div",{'class':'WlYyy diXIH dDKKM'} , recursive=True)
+        # print(reviews_div)
+        # reviews_span=reviews_div.findChildren("span",{'class':'NejBf'} , recursive=True)
+        # reviews_span=reviews_section.findChildren("span",{'class':'NejBf'} , recursive=True)
         rating_svg = reviews_section.findChildren("svg", {'class':'RWYkj d H0'}, recursive=True)
         date_div=reviews_section.findChildren("div", {'class':'WlYyy diXIH cspKb bQCoY'}, recursive=True)
 
-        if(len(reviews_div)==0 or 1==flag):
-            flag=0
-            break
+        # if(len(reviews_span)==0 or 1==flag):
+        #     flag=0
+        #     print("breakkkkkkkkkkkkkkkkkkk11111")
+        #     break
       
-        for r, rs, d in zip(reviews_div, rating_svg, date_div):
+        for r, rs, d in zip(reviews_span, rating_svg, date_div):
             # print(rs['aria-label'])
             if r is None or rs is None or d is None:
                 flag=1
-                break
+                print("breakkkkkkkkkkkkkkkkkkk2")
+                continue
             reviews_list.append(str(r.text.strip()))
             reviews_location.append(location)
             rating_list.append(rs['title'])
@@ -90,6 +106,6 @@ for link in links:
 
         
         dataframe = pd.DataFrame({'location':reviews_location,'content':reviews_list, 'rating':rating_list, 'date':date_list})
-        dataframe.to_csv("./tripadvisor_reviews_nusadua.csv",index=True)#saving to the csv, you can change the name
+        dataframe.to_csv("./tripadvisor_reviews_legian.csv",index=True)#saving to the csv, you can change the name
     
     # print(target_links)
